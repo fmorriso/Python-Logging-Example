eb501157a7c05dab9bf3ad129143a219004fc56f
Fred Morrison <frederick.morrison@outlook.com>
2025-02-22 13:25:13 -0700

    adjust time formatting

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def get_python_version() -> str:
     return f'{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}'
 
 def start_logging():
-    log_format: str = '{time} - {name} - {level} - {function} - {message}'
+    log_format: str = '{time:YYYY-MM-DD HH:mm:ss} - {name} - {level} - {function} - {message}'
     logger.remove()
     logger.add('formatted_log.txt', format = log_format, rotation = '1 MB', retention = '5 days')
     # Add a handler that logs only DEBUG messages to stdout
