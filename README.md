5b62af2ccec1696a2cab5ad4bfbd65da086e1603
Fred Morrison <frederick.morrison@outlook.com>
2025-02-22 13:10:00 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Python Logging Example
+An example of logging using loguru.
+
+## References
+
+* [Loguru on GitHub](https://github.com/Delgan/loguru)
+
+## Tools Used
+
+| Tool      |  Version |
+|:----------|---------:|
+| Python    |   3.13.2 |
+| VSCode    |   1.97.2 |
+| PyCharm   | 2024.3.3 |
+| Loguru | 0.7.3 |
+
+
+## Change History
+
+| Date       | Description                                                                        |
+|:-----------|:-----------------------------------------------------------------------------------|
+| 2025-02-22 | Initial creation                                                                   |
---
+++ main.py
@@ -0,0 +1,25 @@
+import sys
+from loguru import logger
+
+
+def get_python_version() -> str:
+    return f'{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}'
+
+def start_logging():
+    log_format: str = '{time} - {name} - {level} - {function} - {message}'
+    logger.remove()
+    logger.add('formatted_log.txt', format = log_format, rotation = '10 MB', retention = '5 days')
+    # Add a handler that logs only DEBUG messages to stdout
+    logger.add(sys.stdout, level = "DEBUG", filter = lambda record: record["level"].name == "DEBUG")
+
+def main():
+    start_logging()
+
+    logger.debug('top')
+    msg = f'Python version: {get_python_version()}'
+    print(msg)
+    logger.info(msg)
+    logger.debug('bottom')
+
+if __name__ == '__main__':
+    main()
